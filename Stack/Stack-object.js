5390d682d2eb40db928f3885908cf3e4417d06a1
nativeTiger <santakumal41@gmail.com>
2021-12-19 23:36:25 +0545

    stack spelling

--- Stack/Stack-object.js
+++ Stack/Stack-object.js
@@ -1,7 +1,7 @@
 // CREATING A JAVASCRIPT OBJECT-BASED STACK CLASS
 
 /**
- * The easiest way of creating a stck class is using an array to store its elements.
+ * The easiest way of creating a stack class is using an array to store its elements.
  * When working with a large set of data(which is very common in real-world projects),
  * we also need to analyze what is the most efficient way of manipulating the data.
  * When working with arrays, most methods have a complexity of time O(n);
@@ -19,11 +19,11 @@
  *
  */
 class Stack {
-    constructor() {
-        this.count = 0;
-        this.items = {};
-    }
-    // methods
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+  // methods
 }
 
 /**
@@ -32,9 +32,10 @@ class Stack {
  */
 
 // Pushing elements to the stack
-push(element); {
-    this.items[this.count] = element;
-    this.count++;
+push(element);
+{
+  this.items[this.count] = element;
+  this.count++;
 }
 
 const stack = new Stack();
@@ -54,35 +55,38 @@ stack.push(8);
 
 // The count property also works as the size fo the stack. So, for the size method,
 // we can simply return the count property
-size(); {
-    return this.count;
+size();
+{
+  return this.count;
 }
 
 // And to verify whether the stack is empty, we can compare if the count value is 0 as follows:
-isEmpty(); {
-    return this.count === 0;
+isEmpty();
+{
+  return this.count === 0;
 }
 
 // Popping elements from the stack
 /**
  * As we are not using an array to store the elements, we will need to implement
- * the logic to remove an element manually.The pop method also returns the 
+ * the logic to remove an element manually.The pop method also returns the
  * element that was removed from the stack.
  */
-pop(); {
-    if (this.isEmpty()) {
-        return undefined; // we need to verify whether the stack is empty.
-    }
-    this.count--; // if the stack is not empty, we will decrement the count property.
-    const result = this.items[this.count];// we will store the value from the top of the stack, 
-    //so we can return it after the element has been removed.
-    delete this.items[this.count];
-    return result;
+pop();
+{
+  if (this.isEmpty()) {
+    return undefined; // we need to verify whether the stack is empty.
+  }
+  this.count--; // if the stack is not empty, we will decrement the count property.
+  const result = this.items[this.count]; // we will store the value from the top of the stack,
+  //so we can return it after the element has been removed.
+  delete this.items[this.count];
+  return result;
 }
 // let's use the following internal values to emulate the pop action:
 items = {
-    0: 5,
-    1: 8
+  0: 5,
+  1: 8,
 };
 count = 2;
 //To access the element from the top of the stack(latest element added:8),we need to access the key with value 1,
@@ -94,20 +98,22 @@ count = 2;
  * that is stored at the top of the stack, it is necessary to decrement the
  * count property by 1, Sol let's see the code for the peek method:
  */
-peek(); {
-    if (this.isEmpty()) {
-        return undefined;
-    }
-    return this.items[this.count - 1];
+peek();
+{
+  if (this.isEmpty()) {
+    return undefined;
+  }
+  return this.items[this.count - 1];
 }
 
 // And to clear the stack, we can simply reset it to the  same values we used in the constructor:
-clear(); {
-    this.items = {};
-    this.count = 0;
+clear();
+{
+  this.items = {};
+  this.count = 0;
 }
 
 // We could also use the following logic to remove all elements from the stack, respecting the LIFO behavior:
 while (!this.isEmpty()) {
-    this.pop();
+  this.pop();
 }
