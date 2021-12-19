c4a3991613f5e0d79eb2acf31af32b0dd282f750
nativeTiger <santakumal41@gmail.com>
2021-12-19 23:34:25 +0545

    stack of array type

---
+++ Stack/array.js
@@ -0,0 +1,23 @@
+//stack class
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(element) {
+    this.items.push(element);
+  }
+  pop() {
+    return this.items.pop();
+  }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  clearStack() {
+    this.items = [];
+  }
+}
+
+let stack = new Stack();
